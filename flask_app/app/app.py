1b1f29a9a79ddb3f791df02aa69467f026a0a780
ValentinR01 <valentin.rejaunier@hetic.net>
2023-06-05 21:49:24 +0200

    refacto python server config

--- flask_app/app/app.py
+++ flask_app/app/app.py
@@ -1,12 +1,9 @@
-# app.py
 from flask import Flask
  
 app = Flask(__name__)
+
  
 @app.route('/')
 def home():
     return 'hello, world!'
  
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8000)

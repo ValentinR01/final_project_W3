edec2cd6b06645a840f6f07c428710ad714d26ba
Mélissande Dizy <melissandedizy@outlook.com>
2023-06-06 10:05:05 +0200

    premier jet

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-from config import Config
+from .config import config_by_name
+
+db = SQLAlchemy()
+flask_bcrypt = Bcrypt()
 
 
-def create_app(config_class=Config):
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    # Initialize Flask extensions here
-
-    # Register blueprints here
-
-    @app.route('/test/')
-    def test_page():
-        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+    app.config.from_object(config_by_name[config_name])
+    db.init_app(app)
+    flask_bcrypt.init_app(app)
 
     return app

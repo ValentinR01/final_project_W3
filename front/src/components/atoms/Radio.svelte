d2a10af048771c2879853cc342a8d6607d67f236
DFarau <doriane.farau@hetic.net>
2023-06-07 11:37:19 +0200

    add radio atom

---
+++ front/src/components/atoms/Radio.svelte
@@ -0,0 +1,13 @@
+<script>
+/**
+* @type {Array<string>}
+*/
+
+const getValues = [ "italien","anglais","français"]
+
+</script>
+
+{#each getValues as option}
+<input type=radio id={option} value={option} >
+<label for={option}> {option} </label>
+{/each}
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,18 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+  import '../assets/css/global.css'
+  import Radio from '../components/atoms/Radio.svelte'
+
+</script>
+
+<Radio/>
+
+
+
+
+
+
+
+

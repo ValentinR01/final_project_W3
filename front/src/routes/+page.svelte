522025c908756f59041ac5c1d194c6c8f626b4ff
Doustalet Chloé <cdoustalet@gmail.com>
2023-06-07 11:36:22 +0200

    add button atom

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../assets/css/global.css'
+    import Button from '../components/atoms/Button.svelte';
+</script>
+
+<Button> Valider </Button>

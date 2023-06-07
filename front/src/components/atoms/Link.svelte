3fe5091c8ef51b8f2a3b45de670eabaa607522a2
Doustalet Chloé <cdoustalet@gmail.com>
2023-06-07 15:02:51 +0200

    add link atom

---
+++ front/src/components/atoms/Link.svelte
@@ -0,0 +1,24 @@
+<script>
+  /**
+   * @type {string}
+  */
+  export let linkUrl = '#';
+</script>
+
+<a href="{linkUrl}"
+  on:click
+  class="link {$$props.class}">          
+    <slot/>
+</a>
+
+<style>
+  .link{
+    font-family: var(--font-family-primary);
+    font-size: var(--font-size-regular);
+    font-weight: var(--font-weight-regular);
+    color: var(--color-text-dark);
+    text-decoration: none;
+    display: inline-block;
+  }
+</style>
+  

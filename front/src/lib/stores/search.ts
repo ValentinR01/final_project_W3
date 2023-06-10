ef7f975d393f08f38409442f8e2418d0ae0c04b5
Doustalet Chloé <78727498+chloe-dst@users.noreply.github.com>
2023-06-10 17:05:21 +0200

    Update search.ts
    Spaces harmonization in file

--- front/src/lib/stores/search.ts
+++ front/src/lib/stores/search.ts
@@ -1,26 +1,26 @@
 import { writable } from "svelte/store"
 
 export const createSearchStore = (data:any) => {
-	const { subscribe, set, update } = writable({
-		data: data,
-		filtered: data,
-		search: "",
-	})
+  const { subscribe, set, update } = writable({
+    data: data,
+    filtered: data,
+     search: "",
+   })
 
-	return {
-		subscribe,
-		set,
-		update,
-	}
+  return {
+    subscribe,
+    set,
+    update,
+  }
 }
 
 export const searchHandler = (store:any) => {
-	const searchTerm = store.search.toLowerCase() || ""
-	store.filtered = store.data.filter((item:any) => {
-		if (searchTerm != "" ) {
-		  return item.searchTerms.toLowerCase().includes(searchTerm)
-		} else {
-			return
-		}
-	})
+  const searchTerm = store.search.toLowerCase() || ""
+  store.filtered = store.data.filter((item:any) => {
+    if (searchTerm != "" ) {
+      return item.searchTerms.toLowerCase().includes(searchTerm)
+    } else {
+      return
+    }
+  })
 }

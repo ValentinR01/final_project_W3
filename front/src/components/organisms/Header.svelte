663db718ae638c08e7d4ebe27f336e01f4f9a0ac
Doustalet Chloé <cdoustalet@gmail.com>
2023-07-05 11:49:14 +0200

    Add Menu to Header

--- front/src/components/organisms/Header.svelte
+++ front/src/components/organisms/Header.svelte
@@ -2,19 +2,13 @@
   import Image from "../atoms/Image.svelte";
   import Logo from '../../assets/img/logo_color.png'
   import Account from "../../assets/img/account.png";
-  import Link from "../atoms/Link.svelte";
-
-  /**
-   * @type {string}
-  */
-  export let role = 'superadmin';
-  export let domain = 'Traducteur';
-  export let current = 'home';
+  import Menu from "../molecules/Menu.svelte";
 
   /**
   * @type {string | undefined}
   */
    export let employeePicture;
+   export let currentPage = 'home';
    
 </script>
 
@@ -25,58 +19,7 @@
     imageWidth=160
   />
 
-  <ul class='nav'>
-    {#if role == 'superadmin' || role == 'admin' && domain == 'Traducteur'}
-      <Link
-        class='text--uppercase text--semibold {current === 'home' ? 'link--current' : ''}'
-        linkUrl='/dashboard'
-        linkStyle='padding: 0 var(--spacing-3);'
-        linkColor='{current === 'home' ? 'blue' : 'grey'}'
-      > 
-        Les projets
-      </Link>
-
-      <Link
-        class='text--uppercase text--semibold'
-        linkUrl='/dashboard'
-        linkStyle='padding: 0 var(--spacing-3);'
-        linkColor='grey'
-      >
-        Vidéos en ligne
-      </Link>
-    {/if}
-    
-    {#if role == 'superadmin'}
-      <Link
-        class='text--uppercase text--semibold'
-        linkUrl='/dashboard'
-        linkStyle='padding: 0 var(--spacing-3);'
-        linkColor='grey'
-      > 
-        Les utilisateurs
-      </Link>
-    {/if}
-
-    {#if role == 'admin' && domain == 'Regisseur'}
-      <Link
-        class='text--uppercase text--semibold'
-        linkUrl='/dashboard-admin-regi'
-        linkStyle='padding: 0 var(--spacing-3);'
-        linkColor='grey'
-      > 
-        Mes projets
-      </Link>
-
-      <Link
-        class='text--uppercase text--semibold'
-        linkUrl='/dashboard-regi'
-        linkStyle='padding: 0 var(--spacing-3);'
-        linkColor='grey'
-      > 
-        Projets équipe
-      </Link>
-    {/if}
-  </ul>
+  <Menu currentPage={currentPage}/>
 
   <Image
     imageSrc={employeePicture ? employeePicture : Account }
@@ -92,11 +35,5 @@
     margin: var(--spacing-3) var(--spacing-5);
     align-items: center;
     column-gap: var(--spacing-3);
-  }
-
-  .nav{
-    width: calc(100% - 210px);
-    display: flex;
-    justify-content: end;
   }
 </style>

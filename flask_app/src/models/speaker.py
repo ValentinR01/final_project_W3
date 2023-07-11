cd80fe58c6f14067a748d2580b5f50360eadecd3
ValentinR01 <valentin.rejaunier@hetic.net>
2023-07-11 18:24:06 +0200

    Add speaker tests

--- flask_app/src/models/speaker.py
+++ flask_app/src/models/speaker.py
@@ -25,3 +25,7 @@ class Speaker(Base):
         self.publishable = publishable
         self.speaker_parent = speaker_parent
         self.language_id = language_id
+
+    @classmethod
+    def get_all_speaker(cls):
+        return cls.query.all()

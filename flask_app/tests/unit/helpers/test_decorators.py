98cc34713e74b735da8be07291509414f4ba6480
Mélissande Dizy <melissandedizy@outlook.com>
2023-07-12 16:40:13 +0200

    correction des tests du décorator

--- flask_app/tests/unit/helpers/test_decorators.py
+++ flask_app/tests/unit/helpers/test_decorators.py
@@ -1,45 +1,129 @@
-from unittest import mock
+# Import necessary modules and functions
+from unittest.mock import Mock
+
+import pytest
+
+from helpers.auth import AuthHandler
+from models.domain import Domain
+from models.role import Role
 from helpers.decorators import rights_manager
 
 
-def test_rights_manager_authorized():
-    # Mocking the AuthHandler.decode_token method
-    token = "valid_token"
-    role = "admin"
-    domain = "regisseur"
-    decoded_token = {"role": role, "domain": domain}
-
-    with mock.patch("helpers.auth.AuthHandler.decode_token") as \
-            mock_decode_token:
-        mock_decode_token.return_value = decoded_token
-
-        @rights_manager(token, role, domain)
-        def protected_function():
-            return {"message": "success"}
-
-        result = protected_function()
-
-    assert result == {"message": "success"}
+# Set up a mock request object
+class MockRequest:
+    def __init__(self, cookies=None):
+        self.cookies = cookies
 
 
-def test_rights_manager_unauthorized():
-    # Mocking the AuthHandler.decode_token method
-    token = "valid_token"
-    role = "admin"
-    domain = "example.com"
-    decoded_token = {"role": "user", "domain": "example.org"}
+# Define test cases
+def test_rights_manager_no_request():
+    # Create a mock request without cookies
+    request = None
 
-    with mock.patch("helpers.auth.AuthHandler.decode_token") as \
-            mock_decode_token:
-        mock_decode_token.return_value = decoded_token
+    # Define the role and domain parameters for the test
+    role = "worker"
+    domain = "redaction"
 
-        @rights_manager(token, role, domain)
-        def protected_function():
-            return {"message": "success"}
+    # Create a mock function to decorate
+    @rights_manager(request, role, domain)
+    def mock_function():
+        return {"message": "Role passed to decorator doesn't exist"}, 404
 
-        result = protected_function()
+    # Call the decorated function
+    result = mock_function()
 
+    # Assertions
     assert result == (
-        {"message": "unauthorized : the user hasn't the right"},
-        401
+        {"message": "Role passed to decorator doesn't exist"}, 404
     )
+
+
+def test_rights_manager_valid_request():
+    # Create a mock request with valid authorization token
+    authorization_token = "valid_token"
+    request = MockRequest(cookies={"authorization": authorization_token})
+
+    # Define the role and domain parameters for the test
+    role = "worker"
+    domain = "redaction"
+
+    # Create a mock function to decorate
+    @rights_manager(request, role, domain)
+    def mock_function():
+        return "Mock function executed"
+
+    # Mock the necessary dependencies
+    AuthHandler.decode_token = Mock(return_value={"role": "worker",
+                                                  "domain": "redaction"})
+    Role.get_all = Mock(return_value=[Role(name="worker")])
+    Domain.get_all = Mock(return_value=[Domain(name="redaction")])
+
+    # Call the decorated function
+    result = mock_function()
+
+    # Assertions
+    assert result == "Mock function executed"
+
+
+def test_rights_manager_no_role():
+    # Create a mock request with a valid authorization token
+    authorization_token = "valid_token"
+    request = MockRequest(cookies={"authorization": authorization_token})
+
+    # Define the role and domain parameters for the test
+    role = "invalid_role"
+    domain = "redaction"
+
+    # Create a mock function to decorate
+    @rights_manager(request, role, domain)
+    def mock_function():
+        return "Mock function executed"
+
+    # Mock the necessary dependencies
+    AuthHandler.decode_token = Mock(return_value={"role": "worker",
+                                                  "domain": "redaction"})
+    Role.get_all = Mock(return_value=[Role(name="worker")])
+    Domain.get_all = Mock(return_value=[Domain(name="redaction")])
+
+    # Call the decorated function
+    result = mock_function()
+
+    # Assertions
+    assert result == (
+        {"message": "Role passed to decorator doesn't exist"},
+        404
+    )
+
+
+def test_rights_manager_no_domain():
+    # Create a mock request with a valid authorization token
+    authorization_token = "valid_token"
+    request = MockRequest(cookies={"authorization": authorization_token})
+
+    # Define the role and domain parameters for the test
+    role = "worker"
+    domain = "invalid_domain"
+
+    # Create a mock function to decorate
+    @rights_manager(request, role, domain)
+    def mock_function():
+        return "Mock function executed"
+
+    # Mock the necessary dependencies
+    AuthHandler.decode_token = Mock(return_value={"role": "worker",
+                                                  "domain": "redaction"})
+    Role.get_all = Mock(return_value=[Role(name="worker")])
+    Domain.get_all = Mock(return_value=[Domain(name="redaction")])
+
+    # Call the decorated function
+    result = mock_function()
+
+    # Assertions
+    assert result == (
+        {"message": "Domain passed to decorator doesn't exist"},
+        404
+    )
+
+
+# Run the tests
+pytest.main()

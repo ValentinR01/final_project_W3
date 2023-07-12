f5b61573e8893c126cc3cf6d6646e757c554175f
ValentinR01 <valentin.rejaunier@hetic.net>
2023-07-12 22:59:30 +0200

    Fix cookie settings and renew tests

--- flask_app/tests/unit/services.py/test_user_service.py
+++ flask_app/tests/unit/services.py/test_user_service.py
@@ -124,4 +124,4 @@ def test_login_service(mock_login_data, mock_user):
                 print(response)
                 assert response[0] == {'access_token': 'token', 'message': 'Login successful'}
                 assert response[1] == 200
-                assert response[2]['Set-Cookie'].startswith('authorization=token')
+                assert response[2][0][1] == 'authorization=token; max-age=36000; path=/'

305320290ef798e7786f47133e237923cb53fa27
Nassim YAZI <78474187+Nassim-dev@users.noreply.github.com>
2023-08-29 17:57:00 +0200

    feat: test base srv

--- flask_app/tests/unit/services/test_base_service.py
+++ flask_app/tests/unit/services/test_base_service.py
@@ -64,36 +64,53 @@ def test_get_all_entities_no_entity_found():
     with patch('models.base.Base.get_all_by', return_value=[]):
         response, status_code = get_all_entities(MockEntity)
         assert response == {'message': "No entities found"}
+        assert status_code == 204
 
 
 def test_get_all_entities_success():
-    with patch('models.base.Base.get_all_by', return_value=[{"data": "entity1"}]):
+    mock_entity = MagicMock()
+    mock_entity.__dict__ = {"data": "entity1"}
+    mock_data = [mock_entity]
+
+    with patch('models.base.Base.get_all_by', return_value=mock_data):
         response, status_code = get_all_entities(MockEntity)
+        assert response == {'all_mockentity': [{'data': 'entity1'}]}
         assert status_code == 200
-        assert response == {'all_mockentity': [{"data": "entity1"}]}
-#
-# def test_search_entities_no_entity_found():
-#     with patch('models.base.Base.get_entities_by_search_values', return_value=None):
-#         response, status_code = search_entities(MockEntity, "search_val", "col1")
-#         assert status_code == 404
-#         assert response == {'message': "No entities found or it's empty"}
-#
-# def test_search_entities_success():
-#     with patch('models.base.Base.get_entities_by_search_values', return_value=["entity1", "entity2"]):
-#         response, status_code = search_entities(MockEntity, "search_val", "col1")
-#         assert status_code == 200
-#         assert response == {'all_mockentity': ["entity1", "entity2"]}
-#
-# # Tests for get_entity_by_id
-#
-# def test_get_entity_by_id_no_entity_found():
-#     with patch('models.base.Base.get_entity_with_joins', return_value=None):
-#         response, status_code = get_entity_by_id(MockEntity, 1)
-#         assert status_code == 404
-#         assert response == {'message': 'Entity not found'}
-#
-# def test_get_entity_by_id_success():
-#     with patch('models.base.Base.get_entity_with_joins', return_value="entity_data"):
-#         response, status_code = get_entity_by_id(MockEntity, 1)
-#         assert status_code == 200
-#         assert response == {'mockentity': 'entity_data'}
+
+
+def test_search_entities_no_entity_found():
+    with patch(
+            'models.base.Base.get_entities_by_search_values',
+            return_value=None):
+        response, status_code = search_entities(
+            MockEntity, "search_val", "col1"
+        )
+        assert status_code == 204
+        assert response == {'message': "No entities found"}
+
+
+def test_search_entities_success():
+    with patch(
+            'models.base.Base.get_entities_by_search_values',
+            return_value=["entity1", "entity2"]):
+        response, status_code = search_entities(
+            MockEntity, "search_val", "col1"
+        )
+        assert status_code == 200
+        assert response == {'all_mockentity': ["entity1", "entity2"]}
+
+
+def test_get_entity_by_id_no_entity_found():
+    with patch('models.base.Base.get_entity_with_joins', return_value=None):
+        response, status_code = get_entity_by_id(MockEntity, 1)
+        assert status_code == 404
+        assert response == {'message': 'Entity not found'}
+
+
+def test_get_entity_by_id_success():
+    with patch(
+            'models.base.Base.get_entity_with_joins',
+            return_value="entity_data"):
+        response, status_code = get_entity_by_id(MockEntity, 1)
+        assert status_code == 200
+        assert response == {'mockentity': 'entity_data'}

df389d094e168a22808987c6394cfe8547746ace
Mélissande Dizy <melissandedizy@outlook.com>
2023-09-02 13:19:21 +0200

    endpoint language fonctionnel

--- flask_app/src/api/language.py
+++ flask_app/src/api/language.py
@@ -1,18 +1,15 @@
-from flask_restx import Resource, fields, Api
+from flask_restx import Namespace, Resource, fields, Api
 from services.language import get_all_languages
-from api.asset import namespace as namespace
+
+namespace = Namespace('languages', 'Language related endpoints')
 
 api = Api()
 
 language_model = namespace.model(
     'language', {
         'id': fields.Integer(),
-        'content': fields.String,
-        'created_at': fields.DateTime(),
-        'external_name': fields.String(),
-        'posted_by': fields.Integer(),
-        'fullname': fields.String(attribute='user.fullname'),
-        'asset_id': fields.Integer()
+        'name': fields.String,
+        'code': fields.String()
     }
 )
 
@@ -25,7 +22,7 @@ language_list_model = namespace.model(
 
 @namespace.route('/language', methods=["GET"])
 class Language(Resource):
-    # @namespace.marshal_with(language_list_model)
+    @namespace.marshal_with(language_list_model)
     def get(self):
         """Get all languages"""
         return get_all_languages()

92b63bf4d9136d88539433ba33f0f5ac4ced10a7
ValentinR01 <valentin.rejaunier@hetic.net>
2023-09-04 20:44:47 +0200

    Fix base service tests

--- flask_app/tests/unit/services/test_base_service.py
+++ flask_app/tests/unit/services/test_base_service.py
@@ -8,14 +8,6 @@ from services.base import \
     create_entity, get_all_entities, search_entities, get_entity_by_id
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['RESTPLUS_MASK_SWAGGER'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRESQL_DATABASE_URI
-    db.init_app(app)
-    return app
-
-
 class MockEntity(Base):
     __tablename__ = 'mockentity'
     id = db.Column(db.Integer, primary_key=True)
@@ -48,7 +40,8 @@ def test_create_entity_already_exists(mock_entity):
     with patch('models.base.Base.get_by', return_value=mock_entity):
         response, status_code = create_entity(MockEntity, {"data": "value"})
         assert status_code == 409
-        assert response == {'message': 'Entity already exists'}
+        assert response == {'message':
+                            f'{mock_entity.__tablename__} already exists'}
 
 
 def test_create_entity_success(mock_session):
@@ -57,7 +50,8 @@ def test_create_entity_success(mock_session):
             MockEntity, {"data": "value"}
         )
         assert \
-            response == {'message': 'The mockentity created successfully'}
+            response == {'message':
+                         f'The mockentity has been successfully created'}
 
 
 def test_get_all_entities_no_entity_found():

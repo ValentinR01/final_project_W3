7b15ddc8cd658b15c0f29a4589995d20856ac3ea
ValentinR01 <valentin.rejaunier@hetic.net>
2023-09-04 10:08:39 +0200

    Revert "Fix missing comment endpoints"
    This reverts commit 6712ba2cede2e9b4fa029dbee7a942b94a29757c.

--- flask_app/src/blueprints.py
+++ flask_app/src/blueprints.py
@@ -5,7 +5,6 @@ from api.speaker import namespace as speaker
 from api.meta_value import namespace as meta_value
 from api.composer import namespace as composer
 from api.asset import namespace as asset
-from api.comment import namespace as comment # Do not delete this line pls
 
 
 blueprint = Blueprint('api', __name__)
@@ -24,4 +23,5 @@ api.add_namespace(user)
 api.add_namespace(speaker)
 api.add_namespace(meta_value)
 api.add_namespace(composer)
+api.add_namespace(asset)
 api.add_namespace(asset)

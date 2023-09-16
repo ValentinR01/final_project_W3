01398bc01eeeae1bd6e691476d127862ef444bc9
nassim <nassim.yazi2001@gmail.com>
2023-09-16 18:24:02 +0100

    feat: user spe

--- flask_app/src/api/specialty.py
+++ flask_app/src/api/specialty.py
@@ -1,7 +1,6 @@
 from flask_restx import Namespace, Resource, Api
-from services.base import get_all_entities
-from models.specialty import Specialty
-from models.user import specialties_users
+from services.specialty import \
+    get_specialties, get_specialties_by_user, post_user_specialty
 
 
 namespace = Namespace('specialties', 'Specialties by user')
@@ -11,15 +10,22 @@ api = Api()
 
 @namespace.route('/', methods=['GET'])
 class GetAll(Resource):
-    """Get all specialties"""
     def get(self):
         """Get all specialties"""
-        return get_all_entities(entity=Specialty)
+        return get_specialties()
 
 
-@namespace.route('/<int:user_id>', methods=['GET'])
+@namespace.route('/user/<int:user_id>', methods=['GET'])
 class GetByUser(Resource):
-    """Get all specialties by user"""
     def get(self, user_id):
         """Get all specialties by user"""
-        return specialties_users(user_id=user_id)
+        return get_specialties_by_user(user_id)
+
+
+@namespace.route(
+    '/user/<int:user_id>/specialty/<int:specialty_id>', methods=['POST']
+)
+class AddUserToSpecialty(Resource):
+    def post(self, user_id, specialty_id):
+        """Add a new user to a specialty"""
+        return post_user_specialty(user_id, specialty_id)
